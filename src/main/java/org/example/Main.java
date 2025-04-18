e72eb8b634c6a8ff16e8ffbbfe49c6122332bdaf
embet-mengistu <131568421+embet-mengistu@users.noreply.github.com>
2025-04-18 11:10:06 -0700

    added the setters and getters

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,48 @@
 package org.example;
 
-class Book{
+import java.util.Scanner;
+
+class Book {
     int id;
     String isbn;
     String title;
     boolean isCheckOut;
     String checkedOutTo;
 
-    void chekout(){
+    Book(int id, String isbn, String title, boolean isCheckOut, String checkedOutTo) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.isCheckOut = isCheckOut;
+        this.checkedOutTo = checkedOutTo;
 
     }
-    void chekIn(){
+
+    void chekout() {
 
     }
 
+    void chekIn() {
 
+    }
+    //GETTERS
+    public int getId() {return this.id;}
+    public String isbn() {return this.isbn;}
+    public String title() {return this.title;}
+    public boolean isCheckOut() {return this.isCheckOut;}
+    public String checkedOutTo() {return this.checkedOutTo;}
+
+    //SETTERS
+    public void setId(int id) { this.id= id;}
+    public void setIsbn( String isbn) { this.isbn= isbn;}
+    public void setTitle( String title) { this.title= title;}
+    public void setIsCheckOut( boolean isCheckOut) { this.isCheckOut= isCheckOut;}
+    public void setCheckedOutTo( String checkedOutTo) { this.checkedOutTo= checkedOutTo;}
 }
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println();
+        Scanner myScanner = new Scanner(System.in);
     }
 }
